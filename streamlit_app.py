137b53e70789e22edb6dd436e0fd68e64c33d6d5
Moris-Polanco <116950929+Moris-Polanco@users.noreply.github.com>
2023-09-11 02:26:41 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,15 @@
 import openai
 import streamlit as st
-import os
 
-# Autenticación de OpenAI (oculta la clave en una variable de entorno)
+# Configurar la clave de la API de OpenAI
+api_key = st.sidebar.text_input("Enter your OpenAI API key", type="password")
+
 if not api_key:
     st.warning("Please enter a valid API key to continue.")
 else:
     openai.api_key = api_key
+    # Continuar con el resto del código que utiliza la clave de API
+
 # Crear una interfaz de usuario con streamlit
 st.title("Generador de planes de negocios")
 st.caption("Por Moris Polanco")
